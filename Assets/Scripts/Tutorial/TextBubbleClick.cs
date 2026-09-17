4b71103437d70befec87fb1667ac20e868ba8efa
Lamar Jambi <lmj8416@nyu.edu>
2026-09-17 11:04:26 -0400

    added audio for click

--- Assets/Scripts/Tutorial/TextBubbleClick.cs
+++ Assets/Scripts/Tutorial/TextBubbleClick.cs
@@ -5,8 +5,32 @@ public class TextBubbleClick : MonoBehaviour, IPointerClickHandler
 {
     public bool wasClicked = false;
 
+    [Header("Click Sound")]
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _clickSound;
+    [SerializeField] [Range(0f, 1f)] private float _clickVolume = 1f;
+
+    private void Awake()
+    {
+        if (_audioSource == null)
+        {
+            _audioSource = GetComponent<AudioSource>();
+
+            if (_audioSource == null)
+            {
+                _audioSource = gameObject.AddComponent<AudioSource>();
+                _audioSource.playOnAwake = false;
+            }
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (_clickSound != null)
+        {
+            _audioSource.PlayOneShot(_clickSound, _clickVolume);
+        }
+
         wasClicked = true;
     }
 }
